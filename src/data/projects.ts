f41753fa1ea599c88df3ecc6b5dbf3550149c0bc
Kenneth Tran <kenneth.tran@jbhunt.com>
2025-10-25 09:58:03 -0500

    update projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -8,19 +8,26 @@ export type Project = {
 };
 
 const projects: Project[] = [
+    {
+    title: "Let Me Cook AI",
+    techs: ["Angular, Gemini API"],
+    link: "https://https://kennethtran91.github.io/let-me-cook-ai",
+    description: "An intelligent web application designed to turn your available ingredients into delicious recipes using AI recommendation.",
+    image: "https://i.imgur.com/n1DiiOY.png"
+  },
+   {
+    title: "Sweet Land PWA",
+    techs: ["Angular","Firebase","ExpressJS","NodeJS"],
+    link: "https://github.com/kennethtran91/sweet-land-pwa",
+    description: "Simple POS system for small shops owners to manage products and sales.",
+    image: "https://imgur.com/Owws8em.png"
+  },
   {
     title: "BookMe!",
     techs: ["Angular","MongoDB","ExpressJS","NodeJS"],
     link: "https://github.com/kennethtran91/appointment-booking",
     description: "Web app to schedule appointments using MEAN Stacks",
     image: "https://imgur.com/wvHESVX.png"
-  },
-  {
-    title: "Mad Math",
-    techs: ["VueJS"],
-    link: "https://github.com/kennethtran91/MadMath",
-    description: "JavaScript game for kid learning how to do basic math.",
-    image: "https://i.imgur.com/zNWnuwJ.jpg"
   },
   {
     title: "Math Bingo",
